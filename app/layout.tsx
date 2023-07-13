924a28e28d92d45c40cc3e13a9720b30f97d0332
Josh Walter <102444655+StoicGoose@users.noreply.github.com>
2023-07-12 20:39:50 -0600

    Change of Fonts - Changing Project to JWOL

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,20 @@
 import AuthProvider from "@/lib/components/authProvider";
 import HeaderNav from "@/lib/components/navigation/headerNav";
 import "@/lib/styles/global.scss";
-import { Inter } from "next/font/google";
+import { Inter, PT_Serif } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-secondary",
+  display: "swap",
+});
+
+const ptSerif = PT_Serif({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-primary",
+  display: "swap",
+});
 
 export const metadata = {
   title: "Jako's Balay",
@@ -17,12 +28,12 @@ export default function RootLayout({
 }) {
   return (
     <AuthProvider>
-      <html lang="en">
+      <html lang="en" className={`${ptSerif.variable} ${inter.variable}`}>
         <link rel="icon" href="/favicon/JakosBalay-32.png" sizes="32x32" />
         <link rel="icon" href="/favicon/JakosBalay-128.png" sizes="128x128" />
         <link rel="icon" href="/favicon/JakosBalay-180.png" sizes="180x180" />
         <link rel="icon" href="/favicon/JakosBalay-192.png" sizes="192x192" />
-        <body className={inter.className}>
+        <body>
           <div id="modalPortal" />
           <div className="main">
             <HeaderNav />
