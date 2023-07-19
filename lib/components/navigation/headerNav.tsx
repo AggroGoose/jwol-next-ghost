6a342b4e35f641a3448a51755e86c327a9f696f4
Josh Walter <102444655+StoicGoose@users.noreply.github.com>
2023-07-19 09:20:44 -0600

    Redesigned layout to be responsive on mobile

--- lib/components/navigation/headerNav.tsx
+++ lib/components/navigation/headerNav.tsx
@@ -2,11 +2,13 @@ import Link from "next/link";
 import CenterNav from "./centerNav";
 import RightNav from "./rightNav";
 import JoshWithoutLeave from "@/lib/resources/svg/JoshWithoutLeave";
+import MenuNav from "./menuNav";
 
 export default function HeaderNav() {
   return (
     <div className="main-nav">
       <div className="nav-grid">
+        <MenuNav />
         <button className="main-nav--logo">
           <Link href="/">
             <JoshWithoutLeave />
