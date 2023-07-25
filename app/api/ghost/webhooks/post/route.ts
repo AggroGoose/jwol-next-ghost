b13261b1d9fb7f7190b2582c938e6e5ebc81d1ae
Josh Walter <102444655+StoicGoose@users.noreply.github.com>
2023-07-25 14:29:18 -0600

    Adding Console Logs to identify Revalidation Error

--- app/api/ghost/webhooks/post/route.ts
+++ app/api/ghost/webhooks/post/route.ts
@@ -3,12 +3,17 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   if (request.ip !== process.env.WEBHOOK_URL) {
+    console.error("Invalid IP Address on Revalidate: " + request.ip);
     return NextResponse.error();
   }
 
+  console.log("Revalidating Home");
   revalidatePath("/");
+  console.log("Revalidating Journal");
   revalidatePath("/journal");
+  console.log("Revalidating Journal Slug");
   revalidatePath("/journal/[slug]");
+  console.log("Revalidating Journal Tag");
   revalidatePath("/journal/tag/[tag]");
 
   return NextResponse.json({ post: "Well Hello There" });
