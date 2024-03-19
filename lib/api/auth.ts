ef35d86438a760e3b029e1f2476868bbeb7eff9c
StoicGoose <josh@jwolmedia.com>
2024-03-18 23:08:23 -0600

    lowercase google

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/Google";
+import Google from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "./drizzle/";
 
